03456cfd088b825b8062e14d4b3737f997a5c831
Rachit <rachitbhatt2000@gmail.com>
2024-08-21 19:53:44 -0400

    Commit for fraud detection project

--- git_upload_command.bat
+++ git_upload_command.bat
@@ -5,6 +5,6 @@ git add .gitattributes
 git commit -m "Track large files with Git LFS"
 git add .
 git commit -m "Commit for fraud detection project"
-git checkout -b Rachit/Issue1
+git checkout -b main
 git remote add origin https://github.com/rachit-bhatt/Fraud-Detection
-git push origin Rachit/Issue1
+git push origin main
